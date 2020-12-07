b2f6264f33c2da6347ae61795451d1d9e7069ba9
chrisva_co <chvc2003@hotmail.com>
2020-12-06 23:12:42 -0500

    feat: se aade tiempo de vencimiento dinámico

--- tests/Feature/Console/Commands/ActualizarEVencidosTest.php
+++ tests/Feature/Console/Commands/ActualizarEVencidosTest.php
@@ -17,18 +17,20 @@ class ActualizarEVencidosTest extends TestCase
      */
     public function en_tareas_programadas_verificar_que_cambia_a_rechazado_ordenes_vencidas()
     {
-        $fechaVencida = date('Y-m-d H:i:s', strtotime('-' . (config('constants.dias_expiracion') + 1) . ' days'));
+        $fechaVencida = date('Y-m-d H:i:s', strtotime('-' . (config('constants.dias_expiracion')) . ' days '.
+            ' -3 seconds'));
         $fechasVencidas = ['created_at' => $fechaVencida, 'updated_at' => $fechaVencida];
-        //creo 2 ordenes vencidas
+        //creo 2 ordenes vencidas y una no vencida
         $orden1 = Order::factory()->create($fechasVencidas);
         $orden2 = Order::factory()->create($fechasVencidas);
+        $orden3 = Order::factory()->create();
 
         //ejecuto comando
         $comando = new ActualizarEVencidos();
         $comando->handle();
 
         //verifico que el estado sea vencido
-        $ordenesVencidas = Order::whereIn('id',[$orden1->id, $orden2->id])->where('status', Status::ST_REJECTED)->count();
+        $ordenesVencidas = Order::whereIn('id',[$orden1->id, $orden2->id, $orden3->id])->where('status', Status::ST_REJECTED)->count();
         $this->assertSame($ordenesVencidas, 2);
     }
 }
